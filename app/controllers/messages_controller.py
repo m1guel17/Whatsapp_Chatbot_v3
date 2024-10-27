dddd4061b1fd49644bb16e6b69c58da8867c62a1
ignxm <ignx.mfs@gmail.com>
2024-10-27 11:40:06 -0500

    css for all_messages

--- app/controllers/messages_controller.py
+++ app/controllers/messages_controller.py
@@ -5,9 +5,4 @@ def messagesRoutes(app):
     @app.route('/allMessages', methods=['GET', 'POST'])
     def index():
         messages = Message.get_all()
-        last = Message.get_last()
-        print(last)
-        if last is not None:
-            return render_template('index2.html', registros=messages, last = last)
-        else:
-            return render_template('index.html', registros=messages)
+        return render_template('all_messages.html', registros=messages)
--- app/core/crud/read/message_repo.py
+++ app/core/crud/read/message_repo.py
@@ -8,13 +8,12 @@ class MessageRepository:
         return MessagesModel.query.all()
     
     @staticmethod
-    def get_last_message():#phone_number
-        column = getattr(MessagesModel, ColumnNames.NUMBER)
+    def get_last_message():#phone_number # testing
+        #column = getattr(MessagesModel, ColumnNames.NUMBER)
         #return MessagesModel.query.filter_by(column=phone_number).order_by(desc(MessagesModel.id)).first()
         last_row = MessagesModel.query.order_by(MessagesModel.id.desc()).first()
         if last_row is not None:
             return last_row
         else:
             dummy = MessagesModel(id = 1, phone_number = "xxx-xxx-xxx", content = "message", sent_at = "date")
-        
         return dummy
