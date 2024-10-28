a6af49d948cd6f441b0c4809335399f50f7d837c
ignxm <ignx.mfs@gmail.com>
2024-10-27 23:37:14 -0500

    reverse client repo & test core trunk saveMessage

--- app/core/trunk/saveMessage.py
+++ app/core/trunk/saveMessage.py
@@ -6,7 +6,9 @@ def saveText(content, phone_number):
     Client.register(phone_number)
     
     if content == "status":
+        Message.update_by_phone(phone_number, "changed status")
         Client.update_status(phone_number, "client")
     
     elif content == "id":
+        Message.update_by_phone(phone_number, "changed id")
         Client.update_lastOrder_id(phone_number, 12089)
