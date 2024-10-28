f90226d5454ad136d1db35b207df401a8f6a05a7
ignxm <ignx.mfs@gmail.com>
2024-10-27 23:49:09 -0500

    change core>trunk>saveMessage test v2

--- app/core/trunk/saveMessage.py
+++ app/core/trunk/saveMessage.py
@@ -1,14 +1,17 @@
 from app.services.message import Message
 from app.services.client import Client
 
+import time
+
 def saveText(content, phone_number):
     Message.register(phone_number, content)
-    Client.register(phone_number)
+    time.sleep(1)
+    #Client.register(phone_number)
     
     if "status" in content:
         Message.update_by_phone(phone_number, "changed status")
-        Client.update_status(phone_number, "client")
+        #Client.update_status(phone_number, "client")
     
     elif "id" in content:
         Message.update_by_phone(phone_number, "changed id")
-        Client.update_lastOrder_id(phone_number, 12089)
+        #Client.update_lastOrder_id(phone_number, 12089)
