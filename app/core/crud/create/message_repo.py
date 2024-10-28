cbf33b0d91eaf62577e1132f39e7363b702c81dc
ignxm <ignx.mfs@gmail.com>
2024-10-27 19:38:06 -0500

    update crud create message_repo v2

--- app/core/crud/create/message_repo.py
+++ app/core/crud/create/message_repo.py
@@ -10,16 +10,25 @@ class MessageRepository:
         :param message: Instance created from inbound message, contains phone_number and content.
         :param isNew: Boolean variable to discern whether the message instance should be assigned the same chat id or start at 1.
 
-        .. versionchanged:: 1.3
+        .. versionchanged:: 1.4
         """
         if isNew:
             message.chat = 1
             db.session.add(message)
             db.session.commit()
-        else:
-            row = MessagesModel.query.filter_by(phone_number=message.phone_number).first()
-            row.chat = row.chat + 1
-            db.session.add(row)
+            
+            message_ = LastMessageModel(phone_number=message.phone_number, content=message.content)
+            db.session.add(message_)
             db.session.commit()
-        
+            
+        else:
+            #row = MessagesModel.query.filter_by(phone_number=message.phone_number).first()
+            row = MessagesModel.query.filter_by(phone_number=message.phone_number).first()
+            message.chat = row.chat + 1
+            db.session.add(message)
+            db.session.commit()
+            
+            row_ = LastMessageModel.query.filter_by(phone_number=message.phone_number).first()
+            row_.content = message.content
+            db.session.commit()
         
