8138f44a51f5a0f1353d23ce427a9158bf09ad62
ignxm <ignx.mfs@gmail.com>
2024-10-27 19:59:18 -0500

    update crud create message_repo v4

--- app/core/crud/create/message_repo.py
+++ app/core/crud/create/message_repo.py
@@ -11,7 +11,7 @@ class MessageRepository:
         :param content: String variable to update MessagesModel and LastMessageModel with the last inbound message.
         :param isNew: Boolean variable to discern whether the message instance should be assigned the same chat id or start at 1.
 
-        .. versionchanged:: 1.5
+        .. versionchanged:: 1.6
         """
         
         if isNew:
@@ -24,12 +24,12 @@ class MessageRepository:
             db.session.commit()
             
         else:
-            message = MessagesModel.query.filter_by(phone_number=message.phone_number).order_by(MessagesModel.id.desc()).first()
+            message = MessagesModel.query.filter_by(phone_number=phone_number).order_by(MessagesModel.id.desc()).first()
             message.chat = message.chat + 1
             db.session.add(message)
             db.session.commit()
             
-            row_ = LastMessageModel.query.filter_by(phone_number=message.phone_number)
-            row_.content = content
+            message_ = LastMessageModel.query.filter_by(phone_number=phone_number)
+            message_.content = content
             db.session.commit()
         
