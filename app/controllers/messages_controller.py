22713b182a88de588e25c857d1664d97ad55efcc
ignxm <ignx.mfs@gmail.com>
2024-10-28 01:14:10 -0500

    test v7

--- app/controllers/messages_controller.py
+++ app/controllers/messages_controller.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect, url_for
 from app.services.message import Message
 from app.services.client import Client
+from app.models.database.db import db
 
 def messagesRoutes(app):
     @app.route('/allMessages', methods=['GET', 'POST'])
@@ -8,4 +9,14 @@ def messagesRoutes(app):
         messages = Message.get_all()
         lasts = Message.fetch_last_msgs()
         clients = Client.get_all()
-        return render_template('all_messages.html', registros=messages, lasts=lasts, clients=clients)
+        tables_info = {}
+        for table_name, table in db.metadata.tables.items():
+            columns = [{
+                "name": column.name,
+                "type": str(column.type),
+                "nullable": column.nullable,
+                "primary_key": column.primary_key
+            } for column in table.columns]
+            tables_info[table_name] = columns
+            
+        return render_template('all_messages.html', registros=messages, lasts=lasts, clients=clients, tables_info=tables_info)
