473c6a143d429ba13ee4d8c75602a5046373d716
ignxm <ignx.mfs@gmail.com>
2024-11-01 14:43:57 -0500

    new3

--- app/core/trunk/saveMessage.py
+++ app/core/trunk/saveMessage.py
@@ -1,10 +1,10 @@
-#from app.services.message import Message
+from app.services.message import Message
 from app.services.client import Client
 
 import time
 
 def saveText(content, phone_number):
-    #Message.register(phone_number, content)
+    Message.register(phone_number, content)
     time.sleep(1)
     Client.register(phone_number)
     
