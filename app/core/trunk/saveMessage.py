3743a357f874fea06dc361fb5a48e7db542d80ae
ignxm <ignx.mfs@gmail.com>
2024-11-03 14:54:35 -0500

    parse date

--- app/core/trunk/saveMessage.py
+++ app/core/trunk/saveMessage.py
@@ -1,10 +1,5 @@
 from app.services import Message
 from app.services import Client
-# from app.services import Message
-# from app.services.message import Message
-# from app.services.client import Client
-
-import time
 
 def saveText(content, phone_number):
     if Client.isNew(phone_number):
