5c5f5ed63dca1124337e9bf6a003ceece8312cfc
ignxm <ignx.mfs@gmail.com>
2024-11-03 16:14:12 -0500

    changed messages model location

--- app/core/crud/read/message_repo.py
+++ app/core/crud/read/message_repo.py
@@ -1,5 +1,7 @@
-from app.models.orm.databases import MessagesModel
-from app.models.orm.databases import LastMessageModel
+# from app.models.orm.databases import MessagesModel
+from app.models.orm.message import MessagesModel
+# from app.models.orm.databases import LastMessageModel
+from app.models.orm.message import LastMessageModel
 from app import db
 
 class MessageRepository:
--- app/models/orm/message.py
+++ app/models/orm/message.py
@@ -1,24 +1,25 @@
 # from app.models.database.db import db
-# from datetime import datetime
+from app import db
+from datetime import datetime
 
-# class ColumnNames:
-#     NUMBER = "phone_number"
-#     MESSAGE = "content"
-#     CHAT = "chat"
+class ColumnNames:
+    NUMBER = "phone_number"
+    MESSAGE = "content"
+    CHAT = "chat"
 
-# class MessagesModel(db.Model):
-#     __tablename__ = 'messages'
+class MessagesModel(db.Model):
+    __tablename__ = 'messages'
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     phone_number = db.Column(db.String(15), unique=False, nullable=False)
-#     content = db.Column(db.Text, nullable=False)
-#     chat = db.Column(db.Integer, nullable=True)
-#     sent_at = db.Column(db.DateTime, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    phone_number = db.Column(db.String(15), unique=False, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    chat = db.Column(db.Integer, nullable=True)
+    sent_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-# class LastMessageModel(db.Model):
-#     __tablename__ = 'messagesLogs'
+class LastMessageModel(db.Model):
+    __tablename__ = 'messagesLogs'
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     phone_number = db.Column(db.String(15), unique=True, nullable=False)
-#     content = db.Column(db.Text, nullable=False)
-#     sent_at = db.Column(db.DateTime, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    phone_number = db.Column(db.String(15), unique=True, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    sent_at = db.Column(db.DateTime, default=datetime.utcnow)
