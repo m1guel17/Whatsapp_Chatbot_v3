738c01bc6f41276ce5658bd848673fe027923f04
ignxm <ignx.mfs@gmail.com>
2024-11-06 22:29:55 -0500

    self-p2

--- app/stack/web/self_ping.py
+++ app/stack/web/self_ping.py
@@ -1,23 +1,40 @@
 # new method to ping domain
-from apscheduler.schedulers.background import BackgroundScheduler
-import requests
-from app.stack.constant.webdomain import DOMAIN
-
-from app.interfaces.api.whatsapp_api import send_response
 from app.interfaces.generator.msg.json_format import plain_txt
+from app.interfaces.api.whatsapp_api import send_response
+from app.stack.constant.webdomain import DOMAIN
+from datetime import datetime, timedelta
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.date import DateTrigger
+import requests
+import random
 
 def start_scheduler(app):
     scheduler = BackgroundScheduler()
-    scheduler.add_job(func=lambda: ping_app(app), trigger="interval", minutes=2)
+    # scheduler.add_job(func=lambda: ping_app(app), trigger="interval", minutes=2)
     scheduler.start()
+    schedule_next_ping(app, scheduler)
 
-def ping_app(app):
+def schedule_next_ping(app, scheduler):
+    # Generate a random interval (e.g., between 1 and 5 minutes)
+    random_minutes = random.randint(1, 3)
+    next_run_time = datetime.now() + timedelta(minutes=random_minutes)
+
+    # Schedule the ping_app function to run at next_run_time
+    scheduler.add_job(func=lambda: ping_app(app, scheduler, random_minutes),
+                      trigger=DateTrigger(run_date=next_run_time))
+
+def ping_app(app, scheduler, random_minutes):
     with app.app_context():
         try:
             url = DOMAIN.URL + '/health'
             response = requests.get(url)
             print(f'Pinged {url}, Status Code: {response.status_code}')
-            send_response(plain_txt("51998249361", "Ping to server"))
+            send_response(plain_txt("51998249361", "Pinged, Status Code: 200 \nAfter " + random_minutes + " minutes"))
+            # After the ping, schedule the next ping
+            schedule_next_ping(app, scheduler)
         except Exception as e:
             print(f'Error pinging the app: {e}')
             send_response(plain_txt("51998249361", "Ping failed"))
+            # Even if there's an error, schedule the next ping
+            schedule_next_ping(app, scheduler)
