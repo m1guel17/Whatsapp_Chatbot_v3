4b26acd2cd5c55dae04b433f16a182ba326b18e8
ignxm <ignx.mfs@gmail.com>
2024-11-20 19:10:17 -0500

    new feature test

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,26 @@
 from flask import Flask
 from app.models.database.db import db
 from app.config import Config
-from app.stack.web.self_ping import ping_scheduler
+#from app.stack.web.self_ping import ping_scheduler
+from flask_apscheduler import APScheduler
+
+scheduler = APScheduler()
 
 def create_app():
     app = Flask(__name__, template_folder = "./web/templates", static_folder = "./web/static")
     app.config.from_object(Config)
     
     db.init_app(app)
+    scheduler.init_app(app)
+    scheduler.start()
 
     with app.app_context():              
         from app.controllers import Routes
         Routes(app)
         
+        from app.core.jobs.inactive_check import check_inactive_users
+        scheduler.add_job(id='InactivityChecker', func=check_inactive_users, trigger='interval', minutes=1)
+
         db.create_all()
         
         # ping_scheduler(app) # scheduler for ping to the domain
--- app/core/jobs/inactive_check.py
+++ app/core/jobs/inactive_check.py
@@ -9,12 +9,9 @@ from flask import current_app
 def check_inactive_users():
     now = datetime.now()
     timeout = now - timedelta(minutes=3)
-    inactive_users = ClientModel.query.filter(ClientModel.last_interaction < timeout).all()
+    inactive_clients = ClientModel.query.filter(ClientModel.last_interaction < timeout).all()
 
-    for user in inactive_users:
-        # Perform your desired action, e.g., send a reminder message
-        send_response(plain_txt(user.phone_number, "INACTIVE"))
-        
-        # Update the last_interaction to prevent multiple messages
-        user.last_interaction = datetime.now()
+    for client in inactive_clients:
+        send_response(plain_txt(client.phone_number, "INACTIVE"))
+        client.last_interaction = datetime.now()
         db.session.commit()
