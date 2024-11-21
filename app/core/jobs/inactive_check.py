ee664eb0683364cbb733023d006a2fee162c5c9d
ignxm <ignx.mfs@gmail.com>
2024-11-20 19:58:11 -0500

    new feature test 4

--- app/core/jobs/inactive_check.py
+++ app/core/jobs/inactive_check.py
@@ -11,14 +11,14 @@ def inactivity_schedule(app):
     scheduler.add_job(func=lambda: check_inactive_users(app), trigger="interval", minutes=2)
     scheduler.start()
     
-
 def check_inactive_users(app):
     with app.app_context():
         now = datetime.now()
         wait_threshold = datetime.now() - timedelta(minutes=2)
-        inactive_clients = ClientModel.query.filter(ClientModel.last_interaction <= wait_threshold).all()
+        inactive_clients = ClientModel.query.filter(ClientModel.last_interaction <= wait_threshold,  ClientModel.status != "chat inactive").all()
 
         for client in inactive_clients:
             send_response(plain_txt(client.phone_number, "INACTIVE"))
             client.last_interaction = datetime.now()
+            client.status = "chat inactive"
             db.session.commit()
