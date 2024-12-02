c26fbc6f640105cbcbff9f8171e0c139270baee4
ignxm <ignx.mfs@gmail.com>
2024-12-01 22:40:59 -0500

    changes in orm message and html

--- app/models/orm/message.py
+++ app/models/orm/message.py
@@ -25,7 +25,7 @@ class LastMessageModel(db.Model):
     __tablename__ = 'messagesLast'
 
     id = db.Column(db.Integer, primary_key=True)
-    phone_number = db.Column(db.String(15), unique=True, nullable=False)
+    # phone_number = db.Column(db.String(15), unique=True, nullable=False)
     content = db.Column(db.Text, nullable=False)
     sent_at = db.Column(db.DateTime, default=datetime.now)
     status = db.Column(db.Text, nullable=False)
